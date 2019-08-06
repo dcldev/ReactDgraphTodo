7c1940d6cb771f9c535d8a5981d200c07f2f0b5f
Han Seoulo <cu.rogue.coder@gmail.com>
2019-08-06 13:53:49 -0400

    adjusted port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const todoRoutes = express.Router();
-const PORT =  process.env.PORT || 3001;
+const PORT =  process.env.PORT || 4000;
 
 let Todo = require('./todo.model');
 
